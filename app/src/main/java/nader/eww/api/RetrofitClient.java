95bd6348d32fa3465d9e7576f833cbd1f1171bea
nadermkhan <muhammadnadermahbubkhan@gmail.com>
2025-01-31 15:55:22 +0600

    update RetrofitClient.java Fixing Link To API

--- app/src/main/java/nader/eww/api/RetrofitClient.java
+++ app/src/main/java/nader/eww/api/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "https://exuslubricants.xyz/";
+    private static final String BASE_URL = "https://nomoreunskilled.xyz/api/";
     private static Retrofit retrofit = null;
 
     private RetrofitClient() {
